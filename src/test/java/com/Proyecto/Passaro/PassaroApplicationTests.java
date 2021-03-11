1a219a5e0e40259db4701384424e99f89573c08b
cesar gutiérrez <cesargusa-97@hotmail.com>
2021-03-11 17:28:20 +0100

    Initial Proyect

---
+++ src/test/java/com/Proyecto/Passaro/PassaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Passaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
